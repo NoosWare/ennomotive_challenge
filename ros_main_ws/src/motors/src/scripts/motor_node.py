ec0e106510401a1aabc6efa0a03ad37acd7ba224
MariaRamos89 <m.ramos@ortelio.co.uk>
2017-01-21 19:00:38 +0000

    fixed motor control

--- ros_main_ws/src/motors/src/scripts/motor_node.py
+++ ros_main_ws/src/motors/src/scripts/motor_node.py
@@ -3,17 +3,17 @@
 
 import rospy
 import json
-from motor_control import PerformMove, PerformSpin
+from motor_control import PerformMove, PerformSpin, switchOffMotors
 from std_msgs.msg import String
 
 def callback(data):
     speed = json.loads(data.data)
     #print(speed['right_speed'])
     #print(speed['left_speed'])
-    if float(speed['left_speed']) != 100 || float(speed['right_speed']) != 100:
-        PerformMove(float(speed['left_speed']), float(speed['right_speed']), 0.1)
-    else
-        switchOffMotors()
+    #if float(speed['left_speed']) != 100 || float(speed['right_speed']) != 100:
+    PerformMove(float(speed['left_speed']), float(speed['right_speed']), 0.1)
+    #else
+    #switchOffMotors()
         
         
 if __name__ == '__main__':
@@ -21,8 +21,9 @@ if __name__ == '__main__':
     global velocity
     rospy.init_node('motor_controller', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-
+    
     try:
+        switchOffMotors()
         while not rospy.is_shutdown():
             rospy.Subscriber("motors_controller", String, callback)
             rospy.spin()
