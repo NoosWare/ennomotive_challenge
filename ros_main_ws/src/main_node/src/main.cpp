97bdb439f685f6f23af66c9ba5eabc283deebcd9
MariaRamos89 <m.ramos@ortelio.co.uk>
2017-01-27 17:56:02 +0000

    state factory WIP

--- ros_main_ws/src/main_node/src/main.cpp
+++ ros_main_ws/src/main_node/src/main.cpp
@@ -1,20 +1,25 @@
 #include "ros/ros.h"
 #include "robot_action.hpp"
-
+#include "robot_state.hpp"
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "main");
     ros::NodeHandle node;
     auto pub = node.advertise<std_msgs::String>("motors", 1000);
+    auto factory = state_factory(node);
 
-    // TEST: run forward for 3 seconds
+    // TEST: run forward for 3 seconds => get states
     ros::Rate loop_rate(10);
 
     int count = 0;
-    while (ros::ok()){
-
+    while (ros::ok()) 
+    {
+        // create an action
         robot_action a_t(pub, action_type::forward);
         a_t.run();
+
+        // get state and print it on screen
+        factory.get_state().print();
 
         ros::spinOnce();
         loop_rate.sleep();
--- ros_main_ws/src/main_node/src/robot_state.hpp
+++ ros_main_ws/src/main_node/src/robot_state.hpp
@@ -1,40 +1,111 @@
 #ifndef ROBOT_STATE_HPP
 #define ROBOT_STATE_HPP
+#include <string>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "json.hpp"
+
 
 /**
  * @struct robot_state
- * @brief used to describe the current internal and external state of the robot
- * @TODO: obtain this by subscribing to `/laser` and `ping`, and `traffic`
+ * @brief used to describe the current state of the robot
+ * @version 0.1.0
+ * @author Alex Giokas <a.gkiokas@ortelio.co.uk>
+ * @date 27.01.2017
  **/
 struct robot_state
 {
-    double x;
-    double y;
+    float x = 0;
+    float y = 0;
+    float theta = 0;
 
     //qr_info target;
-    bool    traffic;
-    unsigned int ping;
+    //bool    traffic;
+    float ping = 0;
+    float R = 0;
 
-    double R;
-
-    bool operator==(const grid & arg) const
+    bool operator==(const robot_state & arg) const
     {
         return (this->x == arg.x) && 
-               (this->y == arg.y);
+               (this->y == arg.y) &&
+               (this->theta == arg.theta);
+    }
+
+    /// print on stdout
+    void print() const
+    {
+        printf("x: %f y: %f, θ: %f ping %f\r\n", x, y, theta, ping);
     }
 };
 
-class 
+
+/**
+ * @brief create states by observing topics publishing meta-data
+ * @note  subscribes to `coordinates`
+ * @note  subscribes to `imu`
+ * @note  subscribes to `ping`
+ */ 
+class state_factory
+{
+public:
+    state_factory(ros::NodeHandle & node)
+    {
+        coord_sub__ = node.subscribe("coordinates", 
+                                      1000, 
+                                      &state_factory::read_coordinates, 
+                                      this
+                                    );
+        ping_sub__  = node.subscribe("ping", 
+                                      1000, 
+                                      &state_factory::read_ping,
+                                      this);
+    }
+
+    /// parse JSON of {"x":.., "y":..., "thetha":...}
+    void read_coordinates(const std_msgs::String::ConstPtr & msg)
+    {
+        auto obj = nlohmann::json::parse(msg->data);
+        state__.x = obj["x"];
+        state__.y = obj["y"];
+        state__.theta = obj["theta"];
+    }
+
+    /// @brief read ping values from topic `ping`
+    void read_ping(const std_msgs::String::ConstPtr & msg)
+    {
+        state__.ping =  boost::lexical_cast<float>(msg->data);
+    }
+
+    /// @brief read IMU values from topic `imu`
+    void read_imu(const std_msgs::String::ConstPtr & msg)
+    {
+        /// parse JSON of {"acceleration": {"x":..., "y":..., "z":...},
+        ///                "euler": {"yaw":..., "pitch":..., "roll":...}}
+    }
+
+    /// @brief get a copy of the state as it its ATM
+    robot_state get_state() const
+    {
+        return robot_state(state__);
+    }
+
+private:
+    ros::Subscriber coord_sub__;
+    ros::Subscriber ping_sub__;
+    ros::Subscriber imu_sub__;
+    robot_state state__;
+};
 
 namespace std 
 {
 template <> struct hash<robot_state>
 {
-    std::size_t operator()(grid const& arg) const 
+    std::size_t operator()(robot_state const& arg) const 
     {
         std::size_t seed = 0;
         relearn::hash_combine(seed, arg.x);
         relearn::hash_combine(seed, arg.y);
+        relearn::hash_combine(seed, arg.theta);
         return seed;
     }
 };
